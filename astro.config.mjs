f22724666424171edfe2ceedaf6c79f99981f074
RenNogiwa <ren8510.mac@gmail.com>
2025-11-10 22:53:17 +0900

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,46 @@
+import { defineConfig } from 'astro/config';
+import CoverImageDownloader from './src/integrations/cover-image-downloader';
+import CustomIconDownloader from './src/integrations/custom-icon-downloader';
+import FeaturedImageDownloader from './src/integrations/featured-image-downloader';
+import PublicNotionCopier from './src/integrations/public-notion-copier';
+
+const getSite = function () {
+  const CUSTOM_DOMAIN = process.env.CUSTOM_DOMAIN || '';
+  const BASE_PATH = process.env.BASE_PATH || '';
+  
+  if (CUSTOM_DOMAIN) {
+    return new URL(BASE_PATH, `https://${CUSTOM_DOMAIN}`).toString();
+  }
+
+  if (process.env.VERCEL && process.env.VERCEL_URL) {
+    return new URL(BASE_PATH, `https://${process.env.VERCEL_URL}`).toString();
+  }
+
+  if (process.env.CF_PAGES) {
+    if (process.env.CF_PAGES_BRANCH !== 'main') {
+      return new URL(BASE_PATH, process.env.CF_PAGES_URL).toString();
+    }
+
+    return new URL(
+      BASE_PATH,
+      `https://${new URL(process.env.CF_PAGES_URL).host
+        .split('.')
+        .slice(1)
+        .join('.')}`
+    ).toString();
+  }
+
+  return new URL(BASE_PATH, 'http://localhost:3000').toString();
+};
+
+// https://astro.build/config
+export default defineConfig({
+  site: getSite(),
+  base: process.env.BASE_PATH || '',
+  integrations: [
+    CoverImageDownloader(),
+    CustomIconDownloader(),
+    FeaturedImageDownloader(),
+    PublicNotionCopier(),
+  ],
+});
